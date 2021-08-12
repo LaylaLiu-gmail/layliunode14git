7c2185c2847f3b8bf2d2d0dac2ca75f3b1286780
Layla Liu <layliu@microsoft.com>
2021-08-12 09:26:33 +0000

    Updated index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expressbbb' });
+  res.render('index', { title: 'Expressccc' });
 });
 
 module.exports = router;
